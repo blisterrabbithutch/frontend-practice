f20efe12115c75f3b2e5a2a03c5f1a887738a071
Denis Chizh <blisterrabbithutch@gmail.com>
2023-10-01 11:43:56 +0300

    added async lazy route import

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Counter from "./components/Counter";
 import "./index.scss";
+import { Route, Routes, Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 
 const App = () => {
   return (
     <div className="app">
-      asdasdASFASFa
-      <Counter />
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О сайте</Link>
+      <Suspense fallback={<div>Loading Route...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
