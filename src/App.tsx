5d6e7480f96319a2fb6734026389fbf6d54d41bb
Denis Chizh <blisterrabbithutch@gmail.com>
2023-10-09 17:52:19 +0300

    added theme custom hook with context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useContext, useState } from "react";
 import Counter from "./components/Counter";
-import "./index.scss";
+import "./styles/index.scss";
 import { Route, Routes, Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { ThemeContext, Theme } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE THEME</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
       <Suspense fallback={<div>Loading Route...</div>}>
