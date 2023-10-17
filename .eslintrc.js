06d1c6affadc2a4ec4a15e8ba7cb2c36d4363bd5
Denis Chizh <blisterrabbithutch@gmail.com>
2023-10-18 00:43:18 +0300

    Storybook is set up, adapted to TS, absolute paths in environment, css modules, svg support. Added Storybook decorators: StyleDecorator to remove all scss variable imports in component stories. added ThemeDecorator, RouteDecorator. Added stories to widgets, shared, pages

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,11 @@ module.exports = {
         ecmaVersion: 'latest',
         sourceType: 'module',
     },
-    plugins: ['react', '@typescript-eslint', 'i18next'],
+    plugins: [
+        'react',
+        '@typescript-eslint',
+        'i18next',
+    ],
     rules: {
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
@@ -37,10 +41,24 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'i18next/no-literal-string': [
+            'error',
+            {
+                markupOnly: true,
+                ignoreAttribute: ['data-testid', 'to'],
+            },
+        ],
         'max-len': ['error', { ignoreComments: true, code: 100 }],
     },
     globals: {
         __IS_DEV__: true,
     },
+    overrides: [
+        {
+            files: ['**/src/**/*.test.{ts,tsx}'],
+            rules: {
+                'i18next/no-literal-string': 'off',
+            },
+        },
+    ],
 };
